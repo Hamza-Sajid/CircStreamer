c81da58c6ad7b5f9a7f9da08e3abd669034700c8
Humza <pak786hamzasajid66@gmail.com>
2022-06-20 11:21:19 -0700

    Your  message

---
+++ assets/js/custom/landing.js
@@ -0,0 +1,4 @@
+document.querySelector('.js-static-modal-toggle').addEventListener('click', function() {
+    new Modal({el: document.getElementById('static-modal')}).show();
+  });
+"use strict";var KTLandingPage={init:function(){}};"undefined"!=typeof module&&(module.exports=KTLandingPage),KTUtil.onDOMContentLoaded((function(){KTLandingPage.init()}));
